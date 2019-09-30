cc4a1ff1d229fa8525953bc9661b08b4d7329be9
PangDu <xian312117@gmail.com>
2019-09-30 21:43:24 +0800

    Update NSString+URLParameter.m

--- AFOFoundation/string/NSString+URLParameter.m
+++ AFOFoundation/string/NSString+URLParameter.m
@@ -9,13 +9,13 @@
 #import "NSString+URLParameter.h"
 #import "NSString+URLSchemes.h"
 @implementation NSString (URLParameter)
-- (NSString *)addSchemes:(NSString *)url
++ (NSString *)addSchemes:(NSString *)url
                   params:(NSDictionary *)dictionary{
     NSString *baseString = [[NSString readSchemesFromInfoPlist] stringByAppendingString:@"/"];
     baseString = [baseString stringByAppendingString:[NSString readSchemesFromInfoPlist]];
     return [self addQueryStringToUrl:baseString params:dictionary];
 }
-- (NSString*)addQueryStringToUrl:(NSString *)url
++ (NSString*)addQueryStringToUrl:(NSString *)url
                           params:(NSDictionary *)params {
     __block NSMutableString *urlWithQuerystring = [[NSMutableString alloc] initWithString:url];
     // 转换参数
